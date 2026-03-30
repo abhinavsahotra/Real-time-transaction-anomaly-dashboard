9b57b58f727da4fe1b7e78434c0d3dcba72f193e
= <=>
2026-03-30 23:26:08 +0530

    database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,4 +7,15 @@ app.get("/", (req, res) => {
     })
     console.log("Server Started")
 })
+
+app.post('/transaction', ((req, res)=>{
+    // recieve transaction
+    //   const transaction = createTransaction();
+    // create user if not exist
+
+    // run fraud rules
+    // save transaction with risk scores and reason
+    // update users last country
+}))
+console.log(process.env.NODE_ENV);
 app.listen(3000)
